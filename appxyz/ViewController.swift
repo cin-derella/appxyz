c36fa3518c0eead807bebbc3b9d785db68b47f6f
cin-derella <shirley_cinderella@outlook.com>
2019-09-25 21:12:13 +0800

    init

---
+++ appxyz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appxyz
+//
+//  Created by Dante on 2019/9/25.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
